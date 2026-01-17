c383f0e73f9d0f7a4ae89c6fce8f4764e4f5f6f6
-Minh-ng23810310294 <lokisamado@gmail.com>
2026-01-17 16:13:18 +0700

    bai tap buoi 3

---
+++ bai-tap-buoi-2/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.square}>
+        <Text>Hello, world</Text>
+      </View>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  square: {
+    width: 150,
+    height: 150,
+    backgroundColor: 'skyblue',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
